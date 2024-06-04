40fbcd827ad8038d51027754ff2b3ba680ec87ca
Vorobole <vorobyov1903@gmail.com>
2024-06-04 15:18:28 +0300

    hw2

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
+
+export const initMongoConnection = async () => {
+  try {
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+
+    console.log('MongoDB User:', user);
+    console.log('MongoDB Password:', pwd);
+    console.log('MongoDB URL:', url);
+    console.log('MongoDB DB:', db);
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
+  }
+};
+initMongoConnection().catch(console.error);
