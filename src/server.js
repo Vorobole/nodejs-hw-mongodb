dbe071a30d207627f4823ebedc3d3d5cf6f913fe
Vorobole <vorobyov1903@gmail.com>
2024-06-09 12:12:18 +0300

    hw3

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
-import mongoose from 'mongoose';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+// import mongoose from 'mongoose';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -22,44 +24,11 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: res.statusCode,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use(contactsRouter);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(contactId)) {
-      res.status(404).json({
-        message: 'Not found',
-      });
-      return;
-    }
-    const contact = await getContactById(contactId);
+  app.use('*', notFoundHandler);
 
-    if (!contact) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with id ${contactId} not found!`,
-      });
-    }
-
-    res.status(200).json({
-      status: res.statusCode,
-      message: `Successfully found contact with id: ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
